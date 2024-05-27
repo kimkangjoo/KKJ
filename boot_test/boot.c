998f7e4fd5b44054e908e0a2b4043c8f9bbc9014
kimkangjoo <58340104+kimkangjoo@users.noreply.github.com>
2024-05-27 14:55:02 +0900

    Create boot.c

---
+++ boot_test/boot.c
@@ -0,0 +1,16 @@
+#pragma once
+
+void kprintf(char* str, int line)
+{
+	char *video = (char*)(0xB8000 + 160 * line);
+
+	for (int i = 0; str[i] != 0; i++){
+		*video++ = str[i];
+		*video++ = 0x03;
+	}
+	return;
+}
+
+void main(){
+	kprintf("Use function in C!", 10);
+}

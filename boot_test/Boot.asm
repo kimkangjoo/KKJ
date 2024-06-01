84b876f6b6cc37d6694a059db41d6720e6980abd
kimkangjoo <58340104+kimkangjoo@users.noreply.github.com>
2024-06-02 03:01:16 +0900

    Update Boot.asm

--- boot_test/Boot.asm
+++ boot_test/Boot.asm
@@ -1,74 +1,62 @@
 [org 0]
 [bits 16]
 
-jmp 0x07C0:start
+jmp 0x0000:start
 
 start:
-mov ax, cs
-mov ds, ax
+    mov ax, 0x07C0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
 
+    mov si, msg1
+print_loop1:
+    lodsb
+    or al, al
+    jz OS_info
+    mov ah, 0x0E
+    int 0x10
+    jmp print_loop1
 
-mov ax, 0xB800
-mov es, ax
+system_info:
+    mov si, OS_info
+print_loop2:
+    lodsb
+    or al, al
+    jz logo
+    mov ah, 0x0E
+    int 0x10
+    jmp print_loop2
 
-mov byte[es:0], 'h'
-mov byte[es:1], 0x09
-mov byte[es:2], 'i'
-mov byte[es:3], 0x09
+info1:
+    mov si, info_1
+print_loop3:
+    lodsb
+    or al, al
+    jz boot_menu
+    mov ah, 0x0E
+    int 0x10
+    jmp print_loop3
 
-read:
-	mov ax, 0x1000
-	mov es, ax
-	mov bx, 0 
-	mov ah, 2
-	mov al, 2
-	mov ch, 0
-	mov cl, 2
-	mov dh, 0
-	mov dl, 0
-	int 13h
-	
-	jc read
+info2:
+    mov si, info_2
 
-	mov dx, 0x3F2
-	xor al, al
-	out dx, al 
- 
-	cli
+print_loop4:
+    lodsb
+    or al, al
+    jz hang
+    mov ah, 0x0E
+    int 0x10
+    jmp print_loop4
 
-lgdt[gdtr]
-	
-mov eax, cr0
-or eax, 1
-mov cr0, eax
-	
-jmp $+2
-nop
-nop
+hang:
+    jmp hang
 
-mov bx, DataSegment
-mov ds, bx
-mov es, bx
-mov ss, bx
+msg1 db 'Booting from miniOS!', 0
+OS_info db 'OS Information:', 0
+info_1 db '1. Process Scheduling: FCFS, SJF, SRTF, Round Robin', 0
+info_2 db '2. Memory Management: First Fit, Best Fit', 0
 
-jmp dword CodeSegment:0x10000
-
-
-gdtr:
-dw gdt_end - gdt - 1
-dd gdt+0x7C00
-
-gdt:
-
-	dd 0,0
-	CodeSegment equ 0x08
-	dd 0x0000FFFF, 0x00CF9A00
-	DataSegment equ 0x10
-	dd 0x0000FFFF, 0x00CF9200
-	VideoSegment equ 0x18
-	dd 0x8000FFFF, 0x0040920B
-
-gdt_end:
-
-times 510-($-$$) db 0
+times 510-($-$$) db 0      ; 부트 섹터를 0으로 채움
 dw 0xAA55

42b7537d57bbdc133deb3be7f1e167d720a52078
kimkangjoo <58340104+kimkangjoo@users.noreply.github.com>
2024-06-02 01:04:46 +0900

    Update main.c

--- boot_test/main.c
+++ boot_test/main.c
@@ -1,4 +1,14 @@
-#include "function.h"
+
+
+void kprintf(char* str, int line){
+	char *video = (char*)(0xB8000 + 160 * line);
+
+	for (int i = 0; str[i] != 0; i++){
+		*video++ = str[i];
+		*video++ = 0x03;
+	}
+	return;
+}
 
 void main(){
 	kprintf("Use function in C!", 10);

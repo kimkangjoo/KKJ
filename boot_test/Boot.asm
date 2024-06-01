39e7444a263168734fc4eebc60144182cdc02e27
kimkangjoo <58340104+kimkangjoo@users.noreply.github.com>
2024-06-02 03:36:29 +0900

    Update Boot.asm

--- boot_test/Boot.asm
+++ boot_test/Boot.asm
@@ -10,52 +10,63 @@ start:
     mov ss, ax
     mov sp, 0x7C00
 
-    mov si, msg1
+    mov si, msg
 print_loop1:
     lodsb
     or al, al
-    jz system_info
+    jz miniOS_info
     mov ah, 0x0E
     int 0x10
     jmp print_loop1
 
-system_info:
-    mov si, msg2
+miniOS_info:
+    mov si, miniOS_info
 print_loop2:
     lodsb
     or al, al
-    jz info1
+    jz PS_info
     mov ah, 0x0E
     int 0x10
     jmp print_loop2
 
-info1:
-    mov si, msg3
+PS_info:
+    mov si, PS_info
 print_loop3:
     lodsb
     or al, al
-    jz info2
+    jz MM_info
     mov ah, 0x0E
     int 0x10
     jmp print_loop3
 
-info2:
-    mov si, msg4
+MM_info:
+    mov si, MM_info
 print_loop4:
+    lodsb
+    or al, al
+    jz GUI_info
+    mov ah, 0x0E
+    int 0x10
+    jmp print_loop4
+
+GUI_info:
+    mov si, GUI_info
+print_loop5:
     lodsb
     or al, al
     jz hang
     mov ah, 0x0E
     int 0x10
-    jmp print_loop4
+    jmp print_loop5
 
 hang:
     jmp hang
 
-msg1 db 'Booting from miniOS!', 0
-msg2 db 'OS Information:', 0
-msg3 db '1. Process Scheduling: FCFS, SJF, SRTF, Round Robin', 0
-msg4 db '2. Memory Management: First Fit, Best Fit', 0
+msg db 'Booting from miniOS!', 0
+miniOS_info db 'miniOS Information:', 0
+PS_info db '1. Process Scheduling: FCFS, SJF, SRTF, Round Robin', 0
+MM_info db '2. Memory Management: First Fit, Best Fit', 0
+GUI_info db '3. GUI: Login, Input Entry, Output Text, Scroll', 0
 
 times 510-($-$$) db 0
-dw 0xAA55   
+dw 0xAA55
